13f35be0c01b2f1c628ed218abe70a2b35f75404
Bekian <Bekian.Cat@gmail.com>
2023-08-27 23:09:56 -0500

    added temp homeBox code  &insignificant formatting

---
+++ src/lib/+homeBox.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<!-- home box border -->
+<div class="rounded flex box-border  h-fit w-fit p-5 border-4 ">
+    <img class="h-1/4 w-1/4 flex" src="./smudgepfp.jpeg" alt="" >
+    <h2 class="p-5 flex text-3xl text-headerText"> A title!</h2>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
 	const pages = ['home', 'projects', 'about', 'contact']
+    import HomeBox from "$lib/+homeBox.svelte";
 </script>
 
 
 
 <h1 class="text-5xl text-center font-medium text-headerText">A portfolio!</h1>
+<br />
+<HomeBox /> 
 
 <style lang="postcss">
 :global(html) {
