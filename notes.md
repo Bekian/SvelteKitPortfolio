8ebd21fe70e6fe13df8ae6f389c199246e98eddc
Bekian <Bekian.Cat@gmail.com>
2023-08-29 22:49:27 -0500

    fixed homeBox layout

--- notes.md
+++ notes.md
@@ -1,2 +1,3 @@
 ~~1. figure out how to do the page tab thing for the pages~~
 2. add octokit support, octokit has been added just needs a repo component
+3. add dynamic tags to the homeBox so that there can be any positive or zero number n tags
--- src/lib/+homeBox.svelte
+++ src/lib/+homeBox.svelte
@@ -1,9 +1,31 @@
-<script>
+<!--<script>
 
 </script>
 
-<!-- home box border -->
-<div class="rounded flex box-border  h-fit w-fit p-5 border-4 ">
-    <img class="h-1/4 w-1/4 flex" src="./smudgepfp.jpeg" alt="" >
-    <h2 class="p-5 flex text-3xl text-headerText"> A title!</h2>
+<!-- home box border 
+<div class="rounded block box-border h-fit w-fit p-5 border-4 ">
+    <img class="h-1/4 p-0 inline w-1/4 m-0" src="./smudgepfp.jpeg" alt="" >
+    <h2 class="p-5 w-fit inline-block m-0 h-24 text-3xl text-headerText"> A title!</h2>
+    <p class="min-h-fit inline-flex w-3/4 mt-4 text-base text-headerText">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+</div>
+-->
+
+<script>
+</script>
+
+<div class="rounded box-border h-fit w-fit bg-stone-800 border-stone-700 p-5 border-2 flex items-center">
+    <img class="rounded-sm border-stone-700 h-1/4 w-1/4 mr-5" src="./smudgepfp.jpeg" alt="">
+    <div class="flex flex-col">
+        <h2 class="text-3xl text-headerText mb-4">A title!</h2>
+        <p class="mb-4 text-base text-headerText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <div class="flex space-x-2">
+            <span class="px-2 py-1 text-sm bg-gray-300 rounded">Tag 1</span>
+            <span class="px-2 py-1 text-sm bg-gray-300 rounded">Tag 2</span>
+            <span class="px-2 py-1 text-sm bg-gray-300 rounded">Tag 3</span>
+        </div>
+    </div>
 </div>
